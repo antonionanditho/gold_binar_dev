8679b941d4594acd9ed4a0d50cc6f167b15c6aa7
antonionanditho <125547693+antonionanditho@users.noreply.github.com>
2023-04-02 19:17:01 +0700

    Update api.py

--- api.py
+++ api.py
@@ -1,34 +1,49 @@
 import pandas as pd
 import re
-import sqlite3
-from flask import Flask, jsonify
 
-app = Flask(__name__)
-
+from flask import Flask, appcontext_popped, jsonify
 from flask import request
-from flasgger  import Swagger, LazyString, LazyJSONEncoder
+from flasgger import Swagger, LazyString, LazyJSONEncoder
 from flasgger import swag_from
 
-# load data tweet
-data = pd.read_csv("C:/Users/MSI - Dito/Desktop/DSC/Chapter 3/Challenge Chapter 3/Asset Challenge/data.csv", encoding="latin-1")
-# load data kamus alay
-alay = pd.read_csv("C:/Users/MSI - Dito/Desktop/DSC/Chapter 3/Challenge Chapter 3/Asset Challenge/new_kamusalay.csv", encoding="latin-1", header=None)
-alay = alay.rename(columns={0:'Original',
-                            1:'baku'})
-# load data abusive
-abusive = pd.read_csv("C:/Users/MSI - Dito/Desktop/DSC/Chapter 3/Challenge Chapter 3/Asset Challenge/abusive.csv") 
-
+# load data
+alay = pd.read_csv("new_kamusalay.csv", encoding="latin-1", names=("original","replacement"))
+abusive = pd.read_csv("abusive.csv", names=("stopword"))
 # cleaning preprocessing
-def cleaning_data(text):
-    text = text.str.lower()
-    text = re.sub("RT\s","", text)
-    text = re.sub("USER\s","", text)
-    text = re.sub("http[s]?\://\S+","", text)
-    text = re.sub ("^\[x]","", text)
-    text = re.sub('[^0-9a-zA-Z]+',"", text)
+def lowercase(text):
+    return text.lower()
+
+def remove_unnecessary_char(text):
+    text = re.sub('\n',' ',text) # Remove every '\n'
+    text = re.sub('rt',' ',text) # Remove every retweet symbol
+    text = re.sub('user',' ',text) # Remove every username
+    text = re.sub('((www\.[^\s]+)|(https?://[^\s]+)|(http?://[^\s]+))',' ',text) # Remove every URL
+    text = re.sub('  +', ' ', text) # Remove extra spaces
+    return text
+    
+def remove_nonaplhanumeric(text):
+    text = re.sub('[^0-9a-zA-Z]+', ' ', text) 
     return text
 
+alay_dict_map = dict(zip(alay['original'], alay['replacement']))
+def normalize_alay(text):
+    return ' '.join([alay_dict_map[word] if word in alay_dict_map else word for word in text.split(' ')])
 
+def remove_stopword(text):
+    text = ' '.join(['' if word in abusive.values else word for word in text.split(' ')])
+    text = re.sub('  +', ' ', text) # Remove extra spaces
+    text = text.strip()
+    return text
+
+def preprocess(text):
+    text = lowercase(text) # 1
+    text = remove_nonaplhanumeric(text) # 2
+    text = remove_unnecessary_char(text) # 3
+    text = normalize_alay(text) # 4
+    text = remove_stopword(text) # 5
+    return text
+
+# Flask Template
 app.json_encoder = LazyJSONEncoder
 swagger_template = dict(
 info = {
@@ -42,7 +57,7 @@ swagger_config = {
     "headers": [],
     "specs": [
         {
-            "endpoint": '/docs',
+            "endpoint": 'docs',
             "route": '/docs.json',
         }
     ],
@@ -52,10 +67,25 @@ swagger_config = {
 }
 swagger = Swagger(app, template=swagger_template,             
                   config=swagger_config)
+# End Flask Template
 
+@swag_from("docs/text_processing.yml", methods=['POST'])
+@app.route('/text-processing', methods=['POST'])
+def text_processing():
 
-@swag_from("Chapter 3/Challenge Chapter 3/Asset Challenge/doc/text_processing_file.yml", methods=['POST'])
-@app.route('/text_cleaning', methods=['POST'])
+    text = request.form.get('text')
+
+    json_response = {
+        'status_code': 200,
+        'description': "Teks yang sudah diproses",
+        'data': preprocess(text),
+    }
+
+    response_data = jsonify(json_response)
+    return response_data
+
+@swag_from("docs/text_processing_file.yml", methods=['POST'])
+@app.route('/text-processing-file', methods=['POST'])
 def text_processing_file():
 
     # Upladed file
@@ -65,12 +95,12 @@ def text_processing_file():
     df = pd.read_csv(file)
 
     # Ambil teks yang akan diproses dalam format list
-    texts = df.text.to_list()
+    # texts = df.text.to_list()
 
     # Lakukan cleansing pada teks
     cleaned_text = []
-    for text in texts:
-        cleaned_text.append(cleaning_data)
+    for text in df["Tweet"]:
+        cleaned_text.append(preprocess(text))
 
     json_response = {
         'status_code': 200,
